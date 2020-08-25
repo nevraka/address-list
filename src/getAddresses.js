38ceffb3b4008291077c2241547baf87ea0ac249
nevraka <nevrakaya@gmail.com>
2020-08-25 14:50:52 +0200

    split postalcode and city

--- src/getAddresses.js
+++ src/getAddresses.js
@@ -2,12 +2,14 @@ const getAddresses = (text) => {
   return text.split('\n\n').map((block) => {
     const lines = block.split('\n');
     const postCodeCity = lines[lines.length - 1].split(' ');
+    const postCode = postCodeCity[0];
+    const city = postCodeCity.slice(1).join(' ');
 
     return {
       name: lines[0],
       address: lines.length === 3 ? lines[1] : `${lines[1]} ${lines[2]}`,
-      city: postCodeCity[1],
-      postalCode: postCodeCity[0],
+      city: city,
+      postalCode: postCode,
       federalState: null,
     };
   });
