2320e7632c025707636a38fefcabb4c7930ab114
nevraka <nevrakaya@gmail.com>
2021-05-13 21:37:53 +0200

    remove unused function

--- src/utils/getStateFromResults.js
+++ src/utils/getStateFromResults.js
@@ -13,20 +13,3 @@ const getStateFromResults = (results) => {
 };
 
 export default getStateFromResults;
-
-const getStateFromResults2 = (results) => {
-  for (let i = 0; i < results.length; i++) {
-    for (let j = 0; j < results[i].address_components.length; j++) {
-      for (let k = 0; k < results[i].address_components[j].types.length; k++) {
-        if (
-          results[i].address_components[j].types[k] ===
-          'administrative_area_level_1'
-        ) {
-          return results[i].address_components[j].long_name;
-        }
-      }
-    }
-  }
-
-  return 'NOT FOUND';
-};
